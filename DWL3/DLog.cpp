3b52f2643d16e5bea93b15630afea4f1dd40c215
Josep Antoni Bover <devildrey33@hotmail.com>
2021-12-27 19:46:49 +0100

    DWL 3.0010f
    Petit retoc a la funció per imprimir el getlasterror per la consola DLog

--- DWL3/DLog.cpp
+++ DWL3/DLog.cpp
@@ -6,9 +6,6 @@ namespace DWL {
 
 	const BOOL DLog::MostrarUltimoError(void) {
 		WaitForSingleObject(_Mutex, INFINITE);
-		const wchar_t Texto[] = L"GetLastError() : ";
-		LPVOID lpMsgBuf = nullptr;
-		LPVOID lpDisplayBuf = nullptr;
 		DWORD dw = GetLastError();
 		if (dw == 0) {
 			ReleaseMutex(_Mutex);
@@ -16,20 +13,19 @@ namespace DWL {
 			return FALSE;
 		}
 
-		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, dw, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
 
-		if (lpMsgBuf != nullptr) {
-			// Display the error message and exit the process
-			lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, (long)lstrlen((LPCTSTR)lpMsgBuf + (long)lstrlen((LPCTSTR)Texto) + 40l) * sizeof(TCHAR));
-			StringCchPrintf((LPTSTR)lpDisplayBuf, LocalSize(lpDisplayBuf) / sizeof(TCHAR), TEXT("%s failed with error %d: %s\n"), Texto, dw, lpMsgBuf);
-			std::wstring TmpTxt = (LPCTSTR)lpDisplayBuf;
+		static wchar_t Mensaje[4096];
+		Mensaje[0] = 0;
+
+
+		size_t size = FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), Mensaje, (sizeof(Mensaje) / sizeof(wchar_t)), NULL);
+
+
+		if (Mensaje[0] != 0) {
+			std::wstring TmpTxt = L"GetLastError() : ";
+			TmpTxt += (LPCTSTR)Mensaje;
 			EscribirMS(TmpTxt);
 		}
-
-		//		MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
-
-		LocalFree(lpMsgBuf);
-		LocalFree(lpDisplayBuf);
 
 		ReleaseMutex(_Mutex);
 		return TRUE;
